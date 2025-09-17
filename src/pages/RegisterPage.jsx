8d73a016ce4690afb93f515167ba1b8240e571a4
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2025-09-17 09:08:59 -0300

    feat: Integration with Back-end

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useTransition } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Icon from "../assets/images/icon.png";
 import './styles/RegisterPage.css';
+import axios from 'axios';
+import { SignUpValidator } from '../validators/user/SignUpUserValidator';
 
 function RegisterPage() {
+  const navigate = useNavigate();
+  const [isPending, startTransition] = useTransition();
+  
   const [form, setForm] = useState({
+    nome_completo: '',
     email: '',
     nascimento: '',
     telefone: '',
@@ -13,30 +19,117 @@ function RegisterPage() {
   });
 
   const [errors, setErrors] = useState({});
-
+  const [successMessage, setSuccessMessage] = useState("");
+  
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleRegister = () => {
+    const [day, month, year] = form.nascimento.split("/");
+    const date = `${year}-${month}-${day}`;
+
+    startTransition(async () => {
+      const token = axios.get(import.meta.env.VITE_BASE_URL + "authentication/csrf-token/")
+      .then((response) => {console.log(response); console.log(response.data);return response.data})
+      .catch((error) => {console.log(error); setErrors({server: "Erro em requisitar CSRF token para o servidor", details: error}); return;});
+
+      if (!token) {
+        setErrors({server: "Token CSRF não encontrado", details: error})
+        return;
+      };
+
+      const responseSignUp = axios.post(import.meta.env.VITE_BASE_URL + "users/", 
+        {
+          full_name: form.nome_completo,
+          date_birth: date,  
+          email: form.email,
+          phone: form.telefone,
+          password: form.senha
+        },
+        {
+          headers: {
+          "X-CSRFToken": token
+        }
+      })
+      .then(response => {
+        console.log(response);
+
+        if (response.status !== 201) {
+          setErrors({server: "Erro ao registrar usuário!", details: error});
+          return;
+        };
+        
+        setSuccessMessage({server: "Usuário registrado com sucesso!", details: responseSignUp});
+
+        axios.post(import.meta.env.VITE_BASE_URL + "authentication/login/", {
+          email: form.email,
+          password: form.senha
+        },
+        {
+          headers: {
+            "X-CSRFToken": token
+          }
+        })
+        .then(response => {
+          console.log({message: "Login automático realizado com sucesso!"});
+
+        localStorage.setItem("accessToken", response.data.access);
+        localStorage.setItem("refreshToken", response.data.refresh);
+        navigate("/dashboard");
+        })
+        .catch(error => {
+          setErrors({server: "Erro ao realizar login automático!", details: error});
+          console.log({message: "Erro ao realizar login automático!", details: error});
+        });
+
+        return;
+      })
+      .catch(error => {console.log(error); setErrors({server: "Erro ao requisitar o servidor para registrar o usuário!", details: error}); return error});
+
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const newErrors = {};
+    if (!form.nome_completo) newErrors.full_name = 'campo obrigatório*';
     if (!form.email) newErrors.email = 'campo obrigatório*';
-    if (!form.nascimento) newErrors.nascimento = 'campo obrigatório*';
-    if (!form.telefone) newErrors.telefone = 'campo obrigatório*';
-    if (!form.senha) newErrors.senha = 'campo obrigatório*';
-    if (!form.confirmarSenha) newErrors.confirmarSenha = 'campo obrigatório*';
+    if (!form.nascimento) newErrors.date_birth = 'campo obrigatório*';
+    if (!form.telefone) newErrors.phone = 'campo obrigatório*';
+    if (!form.senha) newErrors.password = 'campo obrigatório*';
+    if (!form.confirmarSenha) newErrors.confirmPassword = 'campo obrigatório*';
     if (form.senha && form.confirmarSenha && form.senha !== form.confirmarSenha) {
-      newErrors.confirmarSenha = 'as senhas não coincidem*';
+      newErrors.confirmPassword = 'as senhas não coincidem*';
     }
 
     setErrors(newErrors);
-
+    
     if (Object.keys(newErrors).length === 0) {
-      console.log('Registro enviado com sucesso:', form);
-      
-    }
+      const validate = SignUpValidator.validate({
+            full_name: form.nome_completo,
+            date_birth: form.nascimento,  
+            email: form.email,
+            phone: form.telefone,
+            password: form.senha
+          });
+  
+      if (validate.status === "error") {
+        const fieldErrors = {};
+    
+        for (const [field, messages] of Object.entries(validate.errors)) {
+          fieldErrors[field] = messages.join(", ");
+        }
+  
+        setErrors(fieldErrors);
+        return;
+      };
+    };
+    
+    if (Object.keys(newErrors).length === 0) {
+      handleRegister();
+    };
   };
 
   return (
@@ -46,13 +139,13 @@ function RegisterPage() {
       <form className="form" onSubmit={handleSubmit}>
         <h2>REGISTRO</h2>
 
-        <label>EMAIL</label>
-        <input type="email" name="email" value={form.email} onChange={handleChange} />
-        {errors.email && <span className="erro">{errors.email}</span>}
+        <label>NOME COMPLETO</label>
+        <input type="text" name="nome_completo" value={form.nome_completo} onChange={handleChange} />
+        {errors.full_name && <span className="erro">{errors.full_name}</span>}
 
         <label>DATA NASCIMENTO</label>
-        <input type="date" name="nascimento" value={form.nascimento} onChange={handleChange} />
-        {errors.nascimento && <span className="erro">{errors.nascimento}</span>}
+        <input type="text"  placeholder="DD/MM/AAAA" pattern="\d{2}/\d{2}/\d{4}" name="nascimento" value={form.nascimento} onChange={handleChange} />
+        {errors.date_birth && <span className="erro">{errors.date_birth}</span>}
 
         <label>Telefone</label>
         <input
@@ -66,31 +159,40 @@ function RegisterPage() {
             if (!value.startsWith("+55")) {
               value = "+55" + value.replace(/\D/g, ""); 
             }
-
-           
+            
+            
             if (value.length > 14) {
               value = value.slice(0, 14);
             }
-
+            
             handleChange({
               target: { name: "telefone", value }
             });
           }}
           maxLength={14} 
         />
-        {errors.telefone && <span className="erro">{errors.telefone}</span>}
+        {errors.phone && <span className="erro">{errors.phone}</span>}
+          
+        <label>EMAIL</label>
+        <input type="email" name="email" value={form.email} onChange={handleChange} />
+        {errors.email && <span className="erro">{errors.email}</span>}
 
 
         <label>SENHA</label>
         <input type="password" name="senha" value={form.senha} onChange={handleChange} />
-        {errors.senha && <span className="erro">{errors.senha}</span>}
+        {errors.password && <span className="erro">{errors.password}</span>}
 
         <label>CONFIRME A SENHA</label>
         <input type="password" name="confirmarSenha" value={form.confirmarSenha} onChange={handleChange} />
-        {errors.confirmarSenha && <span className="erro">{errors.confirmarSenha}</span>}
+        {errors.confirmPassword && <span className="erro">{errors.confirmPassword}</span>}
 
-        <button type="submit" className="btn-criar">CRIAR CONTA</button>
+        <button type="submit" className="btn-criar" disabled={isPending}>
+        {isPending ? "Criando conta..." : "CRIAR CONTA"}
+        </button>
       </form>
+
+      {successMessage && <p className="success">{successMessage.message}</p>}
+      {errors.server && <p className="errors-server">{errors.server}{console.log({message: errors.server, details: errors.details})}</p>}
 
       <Link to="/ajuda" className="link-ajuda">Precisando de Ajuda?</Link>
     </div>
