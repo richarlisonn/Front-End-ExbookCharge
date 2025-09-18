006f3d70c5ba2f0d3a723d9baa95d37b974798c2
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2025-09-18 09:23:03 -0300

    feat: Integration SignIn

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import './styles/LoginPage.css';
 import Icon from "../assets/images/icon.png";
+import axios from 'axios';
 
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -22,9 +24,52 @@ function LoginPage() {
       return;
     }
 
-    console.log('Login realizado:', { email, password });
-  };
+    const csrfToken = axios.get(import.meta.env.VITE_BASE_URL + "authentication/csrf-token/")
+    .then((response) => {
+      localStorage.setItem("csrf-token", JSON.stringify(response.data.data));
+      return response.data.data;
+    })
+    .catch((error) => {
+      console.log(error);
+      setErrors({server: "Erro em requisitar CSRF token para o servidor", details: error});
+      return;
+    });
 
+    axios.post(import.meta.env.VITE_BASE_URL + "authentication/login/",
+      {
+        "email": email,
+        "password": password
+      },
+      {
+        headers: {
+          "X-CSRFToken": csrfToken
+        }
+      }
+    )
+    .then((response) => {
+      localStorage.setItem("accessToken", response.data.access);
+      localStorage.setItem("refreshToken", response.data.refresh);
+      
+      const accessToken = localStorage.getItem("accessToken");
+      const refreshToken = localStorage.getItem("refreshToken");
+
+      if (!accessToken) {
+        setErrors({server: "Erro em salvar o token de acesso ou de atualização"})
+      };
+      
+      if (!refreshToken) {
+        setErrors({server: "Erro em salvar o token de acesso ou de atualização"})
+      };
+      
+      navigate("/dashboard");
+    })
+    .catch((error) => {
+      console.log(error);
+      setErrors({server: "Erro em requisitar CSRF token para o servidor", details: error});
+      return;
+    });
+  };
+  
   return (
     <div className="login-container">
       <img src={Icon} alt="Logo" className="logo_LoginPage" />
@@ -60,6 +105,7 @@ function LoginPage() {
         <button type="submit" className="btn-acessar">ACESSAR</button>
       </form>
 
+      {errors.message && <span className="erro">{errors.message}</span>}
       <Link to="/esqueci-senha" className="link-ajuda">Esqueceu a Senha?</Link>
     </div>
   );
