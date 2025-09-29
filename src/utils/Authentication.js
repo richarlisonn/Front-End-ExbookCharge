a5abcc312cdb88ab2c9bfa9ecc7ccd8e24794c8d
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2025-09-29 15:30:19 -0300

    feat: create feature relogin

---
+++ src/utils/Authentication.js
@@ -0,0 +1,42 @@
+const { default: axios } = require("axios");
+
+export class Authentication {
+    static reloginRefreshToken() {
+        const refresh = localStorage.getItem("refreshToken");
+        const csrf = localStorage.getItem("csrf-token");
+
+        if (!refresh) {
+            return {status: "error", message: "Refresh token não encontrado para relogin"};
+        };
+        
+        axios.post(import.meta.env.VITE_API_URL + "authentication/refresh/",
+        {
+            headers: {
+                "X-CSRFToken": csrf,
+            }
+        },
+        {
+            refresh: refresh,
+        })
+        .then((response) => {
+            localStorage.setItem("refreshToken", response.data.refresh);
+            localStorage.setItem("accessToken", response.data.access);
+
+            return {status: "success", message: "Relogin automático realizado com sucesso"};
+        })
+        .catch((error) => {
+            if (error.status === 400) {
+                console.error("Erro ao relogar:", error);
+                return {status: "error", message: "Refresh token inválido, por favor faça login novamente"};
+            };
+
+            if (error.status === 401) {
+                console.error("Erro ao relogar:", error);
+                return {status: "error", message: "Não autorizado, por favor faça login novamente"};
+            };
+
+            console.error("Erro ao relogar:", error);
+            return {status: "error", message: "Ocorreu um erro ao fazer relogin", details: error};
+        });
+    };
+};
