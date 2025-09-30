dbd158ea99a51852f223a98defe6f954cdc17076
richarlisonn <dev.richarlison@gmail.com>
2025-09-30 10:19:59 -0300

    feat: Resolvendo erro, announces era undefined, agr ele se tornou uma array vazio. Deixando de ser undefined.

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useTransition } from "react";
-import Logo from "../assets/images/icon.png"; 
+import Logo from "../assets/images/icon.png";
 import { FaBars, FaUser, FaSearch, FaSignOutAlt } from "react-icons/fa";
-import "./styles/DashBoard.css"; 
+import "./styles/DashBoard.css";
 import axios from "axios";
 import { Authentication } from "../utils/Authentication";
 
@@ -25,39 +25,40 @@ function DashBoard() {
   //   { title: "1984", autor: "George Orwell", link: "/1984" },
   //   { title: "Harry Potter", autor: "J.K. Rowling", link: "/harry-potter" },
   // ]);
-  
+
   const handleGetAnnonces = () => {
     startTransition(() => {
-      const accessToken = localStorage.getItem('accessToken');  
-      const csrfToken =  localStorage.getItem("csrf-token");
+      const accessToken = localStorage.getItem("accessToken");
+      const csrfToken = localStorage.getItem("csrf-token");
 
-      axios.get(import.meta.env.VITE_BASE_URL + 'announces/', {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`,
-          'X-CSRFToken': csrfToken
-        }
-      })
-      .then((response) => {
-        if (response.status === 200) {
-          console.log(response);
-          setAnnounces(response.data);
-        };
-      })
-      .catch((error) => {
-        console.error('Error fetching announces:', error);
+      axios
+        .get(import.meta.env.VITE_BASE_URL + "announces/", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "X-CSRFToken": csrfToken,
+          },
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            console.log(response);
+            setAnnounces(response.data);
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching announces:", error);
 
-        if (error.response.status === 403) {
-          const reloginResponse = Authentication.reloginRefreshToken();
+          if (error.response.status === 403) {
+            const reloginResponse = Authentication.reloginRefreshToken();
 
-          if (reloginResponse.status === "error") {
-            setError(reloginResponse);
-          };
+            if (reloginResponse.status === "error") {
+              setError(reloginResponse);
+            }
 
-          return;
-        };
+            return;
+          }
 
-        setError('Erro ao buscar anúncios: ' + JSON.stringify(error));
-      });
+          setError("Erro ao buscar anúncios: " + JSON.stringify(error));
+        });
     });
   };
 
@@ -67,23 +68,22 @@ function DashBoard() {
 
   return (
     <div className="div_main">
-     
       <header className="header">
-        <FaBars className="icon" onClick={toggleMenu} /> 
+        <FaBars className="icon" onClick={toggleMenu} />
         <a href="/dashboard">
           <img src={Logo} alt="Logo Exbook Change" className="logo_DashBoard" />
         </a>
         <div className="right-icons">
-          <a href="/perfil"><FaUser className="icon" /></a>
+          <a href="/perfil">
+            <FaUser className="icon" />
+          </a>
         </div>
       </header>
 
       <hr className="divider" />
 
-      
       {isMenuOpen && <div className="overlay" onClick={closeMenu}></div>}
 
-     
       <div className={`side-menu ${isMenuOpen ? "open" : ""}`}>
         <div className="menu-header">
           <FaUser className="menu-icon" />
@@ -91,13 +91,20 @@ function DashBoard() {
         </div>
         <hr />
         <ul className="menu-list">
-          <li><a href="/dashboard">Início</a></li>
-          <li><a href="/CriarAnuncio">Criar Anúncio</a></li>
-          <li><a href="/"><FaSignOutAlt /> Sair</a></li>
+          <li>
+            <a href="/dashboard">Início</a>
+          </li>
+          <li>
+            <a href="/CriarAnuncio">Criar Anúncio</a>
+          </li>
+          <li>
+            <a href="/">
+              <FaSignOutAlt /> Sair
+            </a>
+          </li>
         </ul>
       </div>
 
-      
       <div className="search-container">
         <div className="search-input-container">
           <input
@@ -109,16 +116,29 @@ function DashBoard() {
         </div>
       </div>
 
-      
       <div className="ads-container">
-        {announces ? announces.map((livro) => (
-          <a key={livro.id} href={"/" + livro.title} className="ad-card">
-            <div className="ad-image"><img src={livro.images ? import.meta.env.VITE_BASE_URL_IMG + livro.images.find((img) => img.is_cover)?.image : ''} /></div>
-            <div className="ad-info">
-              <h3>{livro.title}</h3>
-            </div>
-          </a>
-        )) : <p>{error ? error : 'Carregando anúncios...'}</p>}
+        {Array.isArray(announces) && announces.length > 0 ? (
+          announces.map((livro) => (
+            <a key={livro.id} href={"/" + livro.title} className="ad-card">
+              <div className="ad-image">
+                <img
+                  src={
+                    livro.images
+                      ? import.meta.env.VITE_BASE_URL_IMG +
+                        livro.images.find((img) => img.is_cover)?.image
+                      : ""
+                  }
+                  alt={livro.title}
+                />
+              </div>
+              <div className="ad-info">
+                <h3>{livro.title}</h3>
+              </div>
+            </a>
+          ))
+        ) : (
+          <p>{error ? error : "Carregando anúncios..."}</p>
+        )}
       </div>
     </div>
   );
