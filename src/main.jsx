d04a72ce1618a590888ca2c0a9c194f46e7c5811
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2025-11-26 13:23:54 -0300

    feat: Dynamic route

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,17 @@ import App from './app.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import DashBoard from './pages/DashBoard.jsx';
-import Anuncio from './pages/Anuncio.jsx'
 import PerfilUsuario from './pages/PerfilUsuario.jsx';  
 import CriarAnuncio from './pages/CriarAnuncio.jsx';
 import EditarPerfil from './pages/EditarPerfil.jsx';
+import AnnouncePage from './pages/AnnouncePage.jsx';
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
   { path: '/register', element: <RegisterPage /> },
   { path: '/login', element: <LoginPage /> },
   { path: '/dashboard', element: <DashBoard /> },
-  { path: '/anuncio', element: <Anuncio /> },
+  { path: '/dashboard/:id', element: <AnnouncePage /> },
   { path: '/perfil', element: <PerfilUsuario /> },
   { path: '/criaranuncio', element: <CriarAnuncio /> },
   { path: '/editarperfil', element: <EditarPerfil /> },
